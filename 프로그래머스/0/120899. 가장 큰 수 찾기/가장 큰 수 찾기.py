fded769d728d612797ecad0f37e64b9f51d78d60
LJU20 <163103141+LJU20@users.noreply.github.com>
2026-06-27 23:10:21 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.00 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
@@ -1,13 +1,4 @@
 def solution(array):
-    tempArray = array[:]
-    array.sort()
-    answer = array[-1]
-    num = 0
-    answerArray = []
-    answerArray.append(answer)
-    for i in tempArray:
-        if i != answer:
-            num += 1
-        else:
-            answerArray.append(num)
-    return answerArray
+    max_num = max(array)
+    index = array.index(max_num)
+    return [max_num, index]
