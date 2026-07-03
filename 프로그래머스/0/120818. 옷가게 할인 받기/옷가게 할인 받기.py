19ff19849f3b795fe6bdb9b5c126ed8e99a6e3ae
LJU20 <163103141+LJU20@users.noreply.github.com>
2026-07-04 02:03:00 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -0,0 +1,5 @@
+def solution(price):
+    if price >= 500000: return int(price*0.8)
+    elif 300000 <= price < 500000: return int(price*0.9)
+    elif 100000 <= price < 300000: return int(price*0.95)
+    else: return price
