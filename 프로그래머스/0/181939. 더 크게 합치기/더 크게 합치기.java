c60e1e87545b2e71d1529182e5438fab9c31fd51
LJU20 <163103141+LJU20@users.noreply.github.com>
2026-07-23 21:04:21 +0900

    [level 0] Title: 더 크게 합치기, Time: 1.91 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,9 +1,5 @@
 class Solution {
     public int solution(int a, int b) {
-        if(Integer.parseInt(String.valueOf(a)+String.valueOf(b)) > Integer.parseInt(String.valueOf(b)+String.valueOf(a))) return Integer.parseInt(String.valueOf(a)+String.valueOf(b));
-        
-    else{
-        return Integer.parseInt(String.valueOf(b)+String.valueOf(a));
-        }
+        return Math.max(Integer.parseInt(String.valueOf(a)+String.valueOf(b)), Integer.parseInt(String.valueOf(b)+String.valueOf(a)));
     }
 }
