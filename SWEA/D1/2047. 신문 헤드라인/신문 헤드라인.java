f4c81b50f875a2bec11d98e0c0d4a4b9caca13af
LJU20 <163103141+LJU20@users.noreply.github.com>
2026-08-18 12:12:10 +0900

    [D1] Title: 신문 헤드라인, Time: 73 ms, Memory: 22,144 KB -BaekjoonHub

---
+++ SWEA/D1/2047. 신문 헤드라인/신문 헤드라인.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Solution {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+        
+		System.out.print(str.toUpperCase());
+	}
+}
