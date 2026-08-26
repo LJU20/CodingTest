27e396587709855ddfbfc5e8225291e62e0edf61
LJU20 <163103141+LJU20@users.noreply.github.com>
2026-08-26 17:00:41 +0900

    [level 1] Title: 모의고사, Time: 0.52 ms, Memory: 84.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+
+class Solution {
+	public ArrayList<Integer> solution(int[] answers) {
+
+		int[] answer1 = { 1, 2, 3, 4, 5 };
+		int[] answer2 = { 2, 1, 2, 3, 2, 4, 2, 5 };
+		int[] answer3 = { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 };
+
+		int res1 = 0;
+		int res2 = 0;
+		int res3 = 0;
+		int number = 0;
+		while (number < answers.length) {
+
+			if (answers[number] == answer1[number % 5]) {
+				res1++;
+			}
+			if (answers[number] == answer2[number % 8]) {
+				res2++;
+			}
+			if (answers[number] == answer3[number % 10]) {
+				res3++;
+			}
+			number++;
+		}
+		ArrayList<Integer> arrlist = new ArrayList<>();
+		if (res1 > res2 && res1 > res3)
+			arrlist.add(1);
+		else if (res2 > res1 && res2 > res3)
+			arrlist.add(2);
+		else if (res3 > res1 && res3 > res2)
+			arrlist.add(3);
+		else if (res1 == res2 && res1 > res3) {
+			arrlist.add(1);
+			arrlist.add(2);
+		} else if (res2 == res3 && res2 > res1) {
+			arrlist.add(2);
+			arrlist.add(3);
+		} else if (res1 == res3 && res1 > res2) {
+			arrlist.add(1);
+			arrlist.add(3);
+		} else if (res1 == res2 && res1 == res3) {
+			arrlist.add(1);
+			arrlist.add(2);
+			arrlist.add(3);
+		}
+
+		return arrlist;
+	}
+}
