ce80bce4244efb5a03e83086f92313b413df69e7
LJU20 <163103141+LJU20@users.noreply.github.com>
2026-08-30 19:13:37 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 80.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+	public int solution(int n) {
+		int answer = 0;
+		ArrayList<Integer> list = new ArrayList<>();
+		while (n > 0) {
+			list.add(n % 10);
+			n /= 10;
+		}
+		for (int i = 0; i < list.size(); i++) {
+			answer += list.get(i);
+		}
+		return answer;
+	}
+}
