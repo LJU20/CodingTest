11e2451439c1d2e73bcd61b6011d0e95fb839c49
LJU20 <163103141+LJU20@users.noreply.github.com>
2026-09-16 17:47:15 +0900

    [D4] Title: 수지의 수지 맞는 여행, Time: 107 ms, Memory: 26,880 KB -BaekjoonHub

--- SWEA/D4/7699. 수지의 수지 맞는 여행/수지의 수지 맞는 여행.java
+++ SWEA/D4/7699. 수지의 수지 맞는 여행/수지의 수지 맞는 여행.java
@@ -10,6 +10,7 @@ public class Solution {
 	static int[] dc = { 0, 0, -1, 1 };
 	static int totalAlpa;
 	static int[] Alpa, visited;
+	static boolean finish;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,6 +22,7 @@ public class Solution {
 			map = new char[r][c];
 			Alpa = new int[26];
 			visited = new int[26];
+			finish = false;
 
 			for (int i = 0; i < r; i++) {
 				String str = br.readLine();
@@ -31,29 +33,32 @@ public class Solution {
 			totalAlpa = 0;
 			for (int i = 0; i < r; i++) {
 				for (int j = 0; j < c; j++) {
-					Alpa[map[i][j] - 'A']++;
+					Alpa[map[i][j] - 'A']++; // 알파벳이 나온 횟수 만큼 1 증가
 				}
 			}
 			for (int i = 0; i < 26; i++) {
-				if (Alpa[i] > 0) {
-					totalAlpa++;
+				if (Alpa[i] > 0) { // 알파벳이 나왔으면
+					totalAlpa++; // 총 알파벳 종류를 카운트
 				}
 			}
 
-			visited[map[0][0] - 'A'] = 1;
+			visited[map[0][0] - 'A'] = 1; // 처음 (0, 0) 알파벳 처리
 			int answer = dfs(0, 0, 1);
 			System.out.printf("#%d %d%n", tc, answer);
 		}
 	}
 
-	static int dfs(int row, int col, int cnt) {
+	static int dfs(int row, int col, int cnt) { // 현재 위치 (row, col), 방문한 알파벳 개수 cnt
 		int result = cnt;
 
+		// 전체 알파벳만큼 나오면 가지치기
 		if (cnt == totalAlpa) {
+			finish = true; // 즉시 반환 시키기 위한 플래그
 			return cnt;
 		}
 
-		for (int i = 0; i < 4; i++) {
+		// 4방위 탐색하면서 알파벳의 개수 cnt 추가
+		for (int i = 0; i < 4 && !finish; i++) {
 			int nr = row + dr[i];
 			int nc = col + dc[i];
 
@@ -61,14 +66,17 @@ public class Solution {
 				continue;
 			}
 
-			int idx = map[nr][nc] - 'A';
-			if (visited[idx] == 1) {
+			int idx = map[nr][nc] - 'A'; // 알파벳을 int로 바꿔서 visited[]에 사용
+			if (visited[idx] == 1) { // 이미 방문한 곳이면 continue
 				continue;
 			}
-            
-			visited[idx] = 1;
-			result = Math.max(result, dfs(nr, nc, cnt + 1));
-			visited[idx] = 0;
+
+			visited[idx] = 1; // 방문한 곳이 아니면 방문처리
+
+			result = Math.max(result, dfs(nr, nc, cnt + 1)); // dfs를 돌면서 더 큰 cnt를 찾으면 result값 갱신
+
+			visited[idx] = 0; // 방문처리 원복
+
 		}
 		return result;
 	}
