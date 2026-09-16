4e212bf0a897926509d2e630bc2f5610618c599b
LJU20 <163103141+LJU20@users.noreply.github.com>
2026-09-16 14:05:27 +0900

    [D6] Title: [S/W 문제해결 응용] 10일차 - 작업순서, Time: 140 ms, Memory: 30,208 KB -BaekjoonHub

--- SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.java
+++ SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
@@ -10,46 +11,53 @@ class Solution {
 	static int[] indegree;
 	static ArrayList<Integer>[] graph;
 	static Deque<Integer> queue;
+	static ArrayList<Integer> list;
 
-	public static void main(String args[]) throws Exception {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		for (int tc = 1; tc <= 10; tc++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			v = Integer.parseInt(st.nextToken());
 			e = Integer.parseInt(st.nextToken());
-
 			graph = new ArrayList[v + 1];
 			indegree = new int[v + 1];
+
+			list = new ArrayList<>();
+			queue = new ArrayDeque<>();
 
 			for (int i = 1; i <= v; i++) {
 				graph[i] = new ArrayList<>();
 			}
+
 			st = new StringTokenizer(br.readLine());
+
 			for (int i = 0; i < e; i++) {
 				int from = Integer.parseInt(st.nextToken());
 				int to = Integer.parseInt(st.nextToken());
+
 				graph[from].add(to);
 				indegree[to]++;
 			}
-			queue = new ArrayDeque<>();
-			System.out.printf("#%d ", tc);
 
 			for (int i = 1; i <= v; i++) {
 				if (indegree[i] == 0) {
 					queue.offer(i);
 				}
 			}
-
 			while (!queue.isEmpty()) {
-				int now = queue.poll();
-				System.out.print(now + " ");
+				int cur = queue.poll();
+				list.add(cur);
 
-				for (int n : graph[now]) {
+				for (int n : graph[cur]) { 
 					indegree[n]--;
 					if (indegree[n] == 0) {
 						queue.offer(n);
 					}
 				}
+			}
+			System.out.print("#" + tc);
+			for (int n : list) {
+				System.out.print(" " + n);
 			}
 			System.out.println();
 		}
