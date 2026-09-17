d154c7714f8a0d5b69bec5e5b280b0b6aaaa0f9b
LJU20 <163103141+LJU20@users.noreply.github.com>
2026-09-17 12:17:21 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 100 ms, Memory: 29,184 KB -BaekjoonHub

---
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+	static int[] parent;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+
+			parent = new int[N + 1];
+			for (int i = 1; i <= N; i++) {
+				makeSet(i);
+			}
+			for (int i = 0; i < M; i++) {
+				st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				union(a, b);
+			}
+			int[] cnt = new int[N + 1];
+			for (int i = 1; i <= N; i++) {
+				cnt[findSet(i)]++;
+			}
+			int totalCnt = 0;
+			for (int i = 1; i <= N; i++) {
+				if (cnt[i] > 0) {
+					totalCnt++;
+				}
+			}
+			System.out.printf("#%d %d%n", tc, totalCnt);
+		}
+	}
+
+	static void makeSet(int x) {
+		parent[x] = x;
+	}
+
+	static int findSet(int x) {
+		if (x == parent[x]) {
+			return x;
+		}
+
+		return parent[x] = findSet(parent[x]);
+	}
+
+	static void union(int a, int b) {
+		int rootA = findSet(a);
+		int rootB = findSet(b);
+
+		if (rootA != rootB) {
+			parent[rootB] = rootA;
+		}
+	}
+}
