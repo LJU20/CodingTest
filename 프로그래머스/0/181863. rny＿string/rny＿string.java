0fb90435edb141679ed559fc9dc5097f4d40c4ff
LJU20 <163103141+LJU20@users.noreply.github.com>
2026-09-20 23:35:19 +0900

    [level 0] Title: rny_string, Time: 0.05 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/0/181863. rny＿string/rny＿string.java
+++ 프로그래머스/0/181863. rny＿string/rny＿string.java
@@ -1,14 +1,5 @@
 class Solution {
     public String solution(String rny_string) {
-        String answer = "";
-        char[] ch = rny_string.toCharArray();
-        for (int i = 0; i < ch.length; i++) {
-			if(ch[i] == 'm') {
-				answer += "rn";
-			} else {
-				answer += ch[i];
-			}
-		}
-        return answer;
+        return rny_string.replace("m", "rn");
     }
 }
